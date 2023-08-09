58f3c4ec76c0671a4682e5d61da589838d38c0ac
BardenAI <135844834+BardenAI@users.noreply.github.com>
2023-08-09 19:43:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,34 @@
-"use strict";
+$(document).ready(function() {
+  // Make the navbar sticky
+  $(".navbar").sticky({ topSpacing: 0 });
 
-window.onload = function () {
-  // Navigation bar buttons
-  var about = document.querySelector("#navAbout");
-  var services = document.querySelector("#navServices");
-  var contact = document.querySelector("#navContact");
+  // Create Recent Posts content dynamically using JavaScript
+  const recentPostsContainer = document.querySelector(".recent-posts .post-list");
 
-  about.onclick = function () {
-    document.querySelector("#aboutRow").scrollIntoView({
-      behavior: "smooth"
-    });
+  const postsData = [
+    { imageSrc: "pic2.png", title: "Adventures in the Mountains", content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
+    { imageSrc: "pic3.png", title: "Urban Exploration", content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." },
+    { imageSrc: "pic4.png", title: "A Glimpse of the Ocean", content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." }
+  ];
+
+  for (const post of postsData) {
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+
+    const postImage = document.createElement("img");
+    postImage.src = post.imageSrc;
+    postImage.alt = post.title;
+
+    const postTitle = document.createElement("h3");
+    postTitle.textContent = post.title;
+
+    const postContent = document.createElement("p");
+    postContent.textContent = post.content;
+
+    postElement.appendChild(postImage);
+    postElement.appendChild(postTitle);
+    postElement.appendChild(postContent);
+
+    recentPostsContainer.appendChild(postElement);
   }
-
-  services.onclick = function () {
-    document.querySelector("#servicesRow").scrollIntoView({
-      behavior: "smooth"
-    });
-  }
-
-  contact.onclick = function () {
-    document.querySelector("#phoneContact").scrollIntoView({
-      behavior: "smooth"
-    });
-  }
-
-  // Learn more jumbotron button jump to about div
-  var bannerBtn = document.querySelector("#bannerBtn");
-  bannerBtn.onclick = function () {
-    document.querySelector("#aboutRow").scrollIntoView({
-      behavior: "smooth"
-    });
-  }
-
-  // Clear input on click in the form
-  var formName = document.querySelector("#formName");
-  formName.onclick = function () {
-    formName.value = "";
-  }
-
-  var formEmail = document.querySelector("#formEmail");
-  formEmail.onclick = function () {
-    formEmail.value = "";
-  }
-
-  var formMessage = document.querySelector("#formMessage");
-  formMessage.onclick = function () {
-    formMessage.value = "";
-  }
-}
+});
